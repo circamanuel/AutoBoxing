93582227761b3b6b81f45440368b9d880ca06c92
circamanuel <manu4126@gmail.com>
2024-12-17 22:18:38 +0100

    upload

---
+++ src/dev/ipa/Main.java
@@ -0,0 +1,60 @@
+package dev.ipa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Integer boxedInt = Integer.valueOf(15);
+        Integer deprecatedBoxing = new Integer(15);
+        int unboxedInt = boxedInt.intValue();
+
+        //  Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+//        System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+        Character[] characterArray = {'a', 'b', 'c', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var ourList = getList(1, 2, 3, 4, 5);
+        System.out.println(ourList);
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs) {
+
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer  returnAnInteger(int i) {
+        return i;
+    }
+
+    private static Double getDoubleObject() {
+
+        return Double.valueOf(100.00);
+    }
+
+    private static double getLiteralDoublePrimitive() {
+
+        return 100.0;
+
+    }
+}
